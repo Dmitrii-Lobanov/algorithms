c48c737d2afad125f6436cf55a275ca9b32c527c
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2020-11-24 18:40:27 +0500

    Update ProblemSolvingPatterns.js

--- ProblemSolvingPatterns.js
+++ ProblemSolvingPatterns.js
@@ -35,3 +35,28 @@ function same(arr1, arr2) {
 }
 
 same([1, 2, 3, 2], [9, 1, 4, 4]);
+
+
+// MULTIPLE POINTERS
+
+/* Write a function called sumZero which accepts a sorted array of integers. 
+The function should find the first pair where the sum is 0. 
+Return an array that includes both values that sum to zero or undefined 
+if a pair does not exist */
+
+const sumZero = (arr: number[]): number[] => {
+  let left = 0;
+  let right = arr.length - 1;
+  while(left < right) {
+    let sum = arr[left] + arr[right];
+    if(sum === 0) {
+      return [arr[left], arr[right]];
+    } else if(sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+sumZero([-4,-3,-2,-1,0, 1,2,5]);
