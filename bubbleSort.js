22a285dbdc380141e55c6116e6bb856176c38825
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2020-11-25 18:01:00 +0500

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,23 +1,36 @@
-const arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+/*
+  A sorting algorithm where the largest values bubble up to the top. 
+  
+  Visualisation: https://visualgo.net/en/sorting
+*/
+
+/* PSEUDOCODE
+  - Start looping with a variable called i from the begi of the array towards the beginning
+  - Start an inner loop with a variable called j from the beginning until i - 1
+  - If arr[j] is greate than arr[j+1], swap those two values
+  - Return the sorted array
+*/ 
+
+function swap(arr, idx1, idx2) {
+  return [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+}
 
 function bubbleSort(arr) {
-  if(arr.length < 2) return arr;
+  // Variable to check if were any swaps in the loop 
+  // to avoid iterations on already sorted array
+  let noSwaps;
   
-  // Included to avoid iterating over sorted array;
-  let swapped;
-  
-  do{
-    swapped = false;
-    for(let i = 0; i < arr.length; i++) {
-      // Swap elements if next is smaller
-      if(arr[i] > arr[i + 1]) {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-        swapped = true;
+  for(let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for(let j = 0; j < i - 1; j++) {
+      if(arr[j] > arr[j+1]) {
+        swap(arr, j, j + 1);
+        noSwaps = false;
       }
     }
-  } while(swapped);
-  
+    if(noSwaps) break;
+  }
   return arr;
 }
 
-bubbleSort(arr);
+bubbleSort([35,12,76,32,1,12]);
