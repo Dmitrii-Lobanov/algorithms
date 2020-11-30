b214b069ebad7df9a8abbd97fb8b35fff9076882
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2020-11-30 14:45:29 +0500

    Update LinkedList-v2.js

--- LinkedList-v2.js
+++ LinkedList-v2.js
@@ -70,4 +70,80 @@ class LinkedList {
     this.length++;
     return this;
   }
+  
+  get(index) {
+    if(index < 0 || index >= this.length) return null;
+    
+    let count = 0;
+    let current = this.head;
+    
+    while(count !== index) {
+      count++;
+      current = current.next;
+    }
+  return current;
+  }
+  
+  set(index, value) {
+    const node = this.get(index);
+    if(node) {
+      node.value = value;
+      return true;
+    }
+    return false;
+  }
+  
+  insert(index, value) {
+    if(index < 0 || index > this.length) return false;
+    if(index === this.length) return !!this.push(value);
+    if(index === 0) return !!this.unshift(value);
+    
+    const previous = this.get(index - 1);
+    const node = new Node(value);
+    node.next = previous.next;
+    previous.next = node;
+    this.length++;
+    return true;
+  }
+  
+  remove(index) {
+    if(index < 0 || index >= this.length) return null;
+    
+    if(index === this.length) return this.pop(index);
+    if(index === 0) return this.shift(index);
+    
+    let previous = this.get(index - 1);
+    let removed = previous.next;
+    previous.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  
+  reverse() {
+    let current = this.head;
+    this.head = this.tail;
+    this.tail = current;
+    
+    let next;
+    let previous = null;
+    
+    for(let i = 0; i < this.length; i++) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+    return this;
+  }
+  
+  // Helper method to represent the list as an array
+  print() {
+    let arr = [];
+    let current = this.head;
+    while(current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }
