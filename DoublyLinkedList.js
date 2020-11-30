788d06f7b000a71a79d016b5d5df5cc740fdc4ee
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2020-11-30 15:25:02 +0500

    Update DoublyLinkedList.js

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -12,6 +12,38 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  
+  push(value) {
+    const node = new Node(value);
+    
+    if(!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.previous = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+  
+ pop() {
+   if(!this.head) return undefined;
+   
+   const popped = this.tail;
+   
+   if(this.length === 1) {
+     this.head = null;
+     this.tail = null;
+   } else {
+     this.tail = popped.previous;
+     this.tail.next = null;
+     popped.previous = null;
+   }
+   this.length--;
+   return popped;
+ }
 }
 
 const dll = new DoublyLinkedList(13);
