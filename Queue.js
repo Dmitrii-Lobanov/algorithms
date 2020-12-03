4ff179d81e572ecf1e10a3e61369a7ae95e37b49
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2020-12-03 17:58:06 +0500

    Update Queue.js

--- Queue.js
+++ Queue.js
@@ -1,23 +1,57 @@
-function Queue() {
-  collection = [];
-  
-  this.enqueue = function(element) {
-    collection.push(element);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  } 
+
+  print() {
+    const res = [];
+    let current = this.first;
+    while(current) {
+      res.push(current.value);
+      current = current.next;
+    }
+    return res;
   }
   
-  this.dequeue = function() {
-    return collection.shift();
+  enqueue(value) {
+    const node = new Node(value);
+    if(this.size === 0) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    this.size++;
+    return node;
   }
   
-  this.print = function() {
-    console.log(collection);
+  dequeue() {
+    if(this.size === 0) return null;
+    let removed = this.first;
+    if(this.size === 1) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return removed;
   }
 }
 
 const queue = new Queue();
-queue.enqueue('1');
-queue.enqueue('2');
-queue.enqueue('3');
+queue.enqueue('first');
+queue.enqueue('second');
+queue.enqueue('third');
+
 queue.dequeue();
 
 queue.print()
