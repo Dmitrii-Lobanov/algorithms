3b53f6b4a93800c3342580e8be580e4f5cf7caf9
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2020-12-03 17:30:55 +0500

    Update Stack.js

--- Stack.js
+++ Stack.js
@@ -1,36 +1,61 @@
-const Stack = function() {
-  this.count = 0;
-  this.storage = {};
-  
-  this.size = function() {
-    return this.count;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  this.push = function(value) {
-    this.storage[this.count] = value;
-    this.count++;
-  };
-  
-  this.pop = function() {
-    if(!this.count) {
-      return undefined;
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  } 
+
+  print() {
+    const res = [];
+    let current = this.first;
+    while(current) {
+      res.push(current.value);
+      current = current.next;
     }
-    this.count--;
-    const popped = this.storage[this.count];
-    delete this.storage[this.count];
-    return popped;
+    return res;
   }
   
-  this.pip = function() {
-    return this.storage;
+  push(value) {
+    const node = new Node(value);
+    if(this.size === 0) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let current = this.first;
+      this.first = node;
+      node.next = current;
+    }
+    this.size++;
+    return node;
+  }
+  
+  pop() {
+    if(this.size === 0) return null;
+    let removed = this.first;
+    if(this.size === 1) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return removed;
   }
 }
 
 const stack = new Stack();
+stack.push('first');
+stack.push('second');
+stack.push('third');
 
-stack.push(1);
-stack.push(2);
-stack.push(3);
 stack.pop();
-stack.size();
-stack.pip();
+stack.pop();
+stack.pop();
+stack.pop();
+
+stack.print()
