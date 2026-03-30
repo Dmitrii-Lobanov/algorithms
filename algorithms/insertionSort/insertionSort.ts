65552910d50505702ef68ebe0dbac7db996d6642
Dmitrii <dmitriilobanov3@gmail.com>
2026-03-30 10:24:04 -0500

    Insertion Sort

--- algorithms/insertionSort/insertionSort.ts
+++ algorithms/insertionSort/insertionSort.ts
@@ -2,15 +2,14 @@ export function insertionSort(arr: number[]) {
   if (!arr.length || arr.length === 1) return arr;
 
   for (let i = 1; i < arr.length; i++) {
-    let j = i - 1;
-    let tmp = arr[i];
+    let j;
+    let numberToInsert = arr[i];
 
-    while (j >= 0 && arr[j] > tmp) {
+    for (j = i - 1; arr[j] > numberToInsert; j--) {
       arr[j + 1] = arr[j];
-      j--;
     }
 
-    arr[j + 1] = tmp;
+    arr[j + 1] = numberToInsert;
   }
 
   return arr;
